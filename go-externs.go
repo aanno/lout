b2c4d7b2b3d93598943cef455b6d17fe4d85eea3
aanno <aanno@users.noreply.github.com>
2022-10-23 15:22:02 +0200

    begin with style, context, gap

---
+++ go-externs.go
@@ -0,0 +1,80 @@
+package main
+
+// #cgo LDFLAGS: -lz -lm -L. -llout
+//
+// #include <stdio.h>
+// #include <stdlib.h>
+//
+// #include "go_linux_64.h"
+// #include "./externs.h"
+//
+import "C"
+import (
+	"bytes"
+	"fmt"
+	"os"
+	"unsafe"
+)
+
+type OBJECT = unsafe.Pointer
+type FULL_LENGTH = int32
+type FONT_NUM = uint16
+type COLOUR_NUM = uint16
+type TEXTURE_NUM = uint16
+type LANGUAGE_NUM = uint32
+
+/*@::GAP, STYLE@**************************************************************/
+/*                                                                           */
+/*  typedef GAP - what separates one object from another.                    */
+/*                                                                           */
+/*****************************************************************************/
+
+type gap_t struct { 
+	owidth	FULL_LENGTH	/* width of the gap                  */
+	onobreak bool		/* TRUE if this gap is unbreakable   */
+  	omark bool			/* TRUE if this gap is marked        */
+	ojoin bool			/* TRUE if joins exist across gap    */
+	ounits uint8		/* :3 units of measurement: fixed, etc  */
+	omode uint8			/* :3 spacing mode: edge-to-edge, etc   */
+}
+
+/* A key-value pair.  */
+type context_t struct {
+	okey *OBJECT			/* name of a context variable        */
+	ovalue *OBJECT			/* associated value		     */
+	oenv *OBJECT			/* environment for this value        */
+	ostyle *style_t			/* style for this value              */
+}
+
+type style_t struct {
+	oline_gap gap_t			/* separation between lines          */
+	ospace_gap gap_t		/* separation induced by white space */
+	oyunit FULL_LENGTH		/* value of y unit of measurement    */
+	ozunit FULL_LENGTH		/* value of z unit of measurement    */
+	ooutdent_len FULL_LENGTH		/* amount to outdent in outdent style*/
+	osmallcaps_len FULL_LENGTH		/* size of small capitals            */
+	ofont FONT_NUM			/* current font                      */
+	ocolour COLOUR_NUM		/* current colour		     */
+	ounderline_colour COLOUR_NUM	/* current underline colour	     */
+	otexture TEXTURE_NUM	/* current texture		     */
+	oblanklinescale uint16	/* scale factor for blank lines      */
+	olanguage LANGUAGE_NUM	/* :6 current language		     */
+	ovadjust bool			/* :1 @VAdjust in effect                */
+	ohadjust bool			/* :1 @HAdjust in effect                */
+	opadjust bool			/* :1 @PAdjust in effect                */
+	osmall_caps bool		/* :1 small capitals                    */
+	ospace_style uint8		/* :3 space style: lout, troff, tex, .. */
+	ohyph_style uint8		/* :2 hyphenation off or on             */
+	ofill_style uint8		/* :2 fill lines with text off/on       */
+	odisplay_style uint8			/* :3 display lines adjusted, ragged... */
+	ooutline bool			/* :2 TRUE if outlining words           */
+	onobreakfirst bool		/* :1 no break after first line of para */
+	onobreaklast bool		/* :1 no break after last line of para  */
+	obaselinemark bool		/* :1 baseline char metrics             */
+	ostrut bool				/* :1 strut char metrics                */
+	oligatures bool			/* :1 use ligatures                     */
+	omarginkerning bool		/* :1 perform margin kerning            */
+	ocontext context_t		/* context stack		     */
+}
+
+
